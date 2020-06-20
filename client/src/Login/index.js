580730d03c59163018bb6d28981a5a7e138bc205
Dima Ostapchuk <ostapchukvox@gmail.com>
2020-06-21 00:10:49 +0300

    add login page

--- client/src/Login/index.js
+++ client/src/Login/index.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React, { useState } from "react";
 
 export const Login = () => {
-    return (
-        <h1>Login page:</h1>
-    );
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function validateForm() {
+    return email.length > 0 && password.length > 0;
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+            <label htmlFor="email">Email</label>
+            <input value={email} onChange={e => setEmail(e.target.value)} type="email" placeholder="Enter Email" name="email" required></input>
+
+            <label htmlFor="psw">Password</label>
+            <input value={password} onChange={e => setPassword(e.target.value)} type="password" placeholder="Enter Password" name="psw" required></input>
+
+            <input disabled={!validateForm()} type="submit" value="Login"></input>
+        </form>
+    </div>
+  );
 }
