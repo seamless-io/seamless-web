49581387dbb444cebb221f40857fe0078ff411d1
andheroe <freefunning@gmail.com>
2020-06-28 02:51:53 +0300

    fix

--- core/api_key.py
+++ core/api_key.py
@@ -6,11 +6,12 @@ import secrets
 API_KEY_LENGTH = 20
 
 
-def generate_api_key():
+def generate_api_key() -> str:
     """
     Generates an API key.
     Returns:
-        str
+        Return a random text string, in hexadecimal.
+        The string has API_KEY_LENGTH/2 random bytes, each byte converted to two hex digits.
     """
-    api_key = secrets.token_urlsafe(API_KEY_LENGTH)
+    api_key = secrets.token_hex(API_KEY_LENGTH / 2)
     return api_key
