3af2f3b40b5311d82852fb7e08affe153980493a
andheroe <freefunning@gmail.com>
2020-06-28 02:54:34 +0300

    fix

--- core/api_key.py
+++ core/api_key.py
@@ -3,7 +3,7 @@ Generates a user API key
 """
 import secrets
 
-API_KEY_LENGTH = 20
+API_KEY_LENGTH = 20  # Must be dividable by 2 !!!
 
 
 def generate_api_key() -> str:
@@ -13,5 +13,5 @@ def generate_api_key() -> str:
         Return a random text string, in hexadecimal.
         The string has API_KEY_LENGTH/2 random bytes, each byte converted to two hex digits.
     """
-    api_key = secrets.token_hex(API_KEY_LENGTH / 2)
+    api_key = secrets.token_hex(int(API_KEY_LENGTH / 2))
     return api_key
