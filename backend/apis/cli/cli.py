320cb63221a2da09b766b7bb41560d9cc16febd2
andheroe <freefunning@gmail.com>
2020-06-30 13:45:24 +0300

    upd

--- backend/apis/cli/cli.py
+++ backend/apis/cli/cli.py
@@ -3,35 +3,58 @@ from flask import Blueprint, jsonify, request, Response
 from backend.db import session_scope
 from backend.db.models import Job, User
 from job_executor import project, executor
-from job_executor.project import ProjectType
+from job_executor.project import JobType
 
 cli_bp = Blueprint('cli', __name__)
 
 
-@cli_bp.route('/publish', methods=['POST'])
+@cli_bp.route('/publish', methods=['PUT'])
 def create_job():
     api_key = request.headers.get('Authorization')
     if not api_key:
         return Response('Not authorized request', 401)
 
+    file = request.files.get('seamless_project')
+    if not file:
+        return Response('File not provided', 400)
+
     job_name = request.args.get('name')
     cron_schedule = request.args.get('schedule')
 
-    print(job_name)
-    print(cron_schedule)
-
     with session_scope() as session:
         user = User.get_user_from_api_key(api_key, session)
-        job_attributes = {
-            'name': job_name,
-            'user_id': user.id
-        }
-        if cron_schedule:
-            job_attributes.update({"schedule": cron_schedule})
-        job = Job(**job_attributes)
-        session.add(job)
-        session.commit()
-        return jsonify({'job_id': job.id}), 200
+        existing_job = None
+        for j in user.jobs:
+            if j.name == job_name:
+                existing_job = j
+                break
+        if existing_job:  # The user re-publishes an existing job
+            job_id = existing_job.id
+        else:  # The user publishes the new job
+            job_attributes = {
+                'name': job_name,
+                'user_id': user.id
+            }
+            if cron_schedule:
+                job_attributes.update({"schedule": cron_schedule})
+            job = Job(**job_attributes)
+            session.add(job)
+            session.commit()
+            job_id = job.id
+
+    try:
+        project_path = project.create(file,
+                                      api_key,
+                                      JobType.PUBLISHED,
+                                      str(job_id))
+    except project.ProjectValidationError as exc:
+        return Response(str(exc), 400)
+
+    print(job_name)
+    print(cron_schedule)
+    print(project_path)
+
+    return jsonify({'job_id': job_id}), 200
 
 
 @cli_bp.route('/run', methods=['POST'])
@@ -45,7 +68,7 @@ def run() -> Response:
         return Response('File not provided', 400)
 
     try:
-        project_path = project.create(file, api_key, ProjectType.RUN)
+        project_path = project.create(file, api_key, JobType.RUN)
     except project.ProjectValidationError as exc:
         return Response(str(exc), 400)
 
--- job_executor/project.py
+++ job_executor/project.py
@@ -9,6 +9,7 @@ import datetime
 import logging
 from enum import Enum
 from pathlib import Path
+from typing import Optional
 
 from werkzeug.datastructures import FileStorage
 
@@ -18,31 +19,39 @@ UPLOAD_FOLDER = "user_projects"
 DATETIME_FOLDER_NAME_FORMAT = "%m_%d_%Y_%H_%M_%S"
 
 
-class ProjectType(Enum):
+class JobType(Enum):
     RUN = 'run'
     PUBLISHED = 'published'
 
 
-def create(fileobj: FileStorage, api_key: str, project_type: ProjectType):
+def get_path_to_job(job_type: JobType,
+                    api_key: str,
+                    job_id: Optional[str]):
+    user_folder_path = str(os.path.join(UPLOAD_FOLDER, job_type.value, api_key))
+    if job_type == JobType.PUBLISHED:
+        # Add project because there may be multiple published projects
+        user_folder_path = str(os.path.join(user_folder_path, job_id))
+    return user_folder_path
+
+
+def create(fileobj: FileStorage,
+           api_key: str,
+           job_type: JobType,
+           job_id: str = None):
     if fileobj.filename and not fileobj.filename.endswith(ALLOWED_EXTENSION):
         raise ProjectValidationError('File extension is not allowed')
 
-    dest = _save_to_disk(fileobj, api_key, project_type)
-    return dest
-
-
-def _save_to_disk(fileobj: FileStorage, api_key: str, project_type: ProjectType) -> str:
-    user_folder_path = str(os.path.join(UPLOAD_FOLDER, project_type.value, api_key))
-    if os.path.exists(user_folder_path):
-        shutil.rmtree(user_folder_path)  # Remove previously created project
-    Path(user_folder_path).mkdir(parents=True, exist_ok=True)
+    path = get_path_to_job(job_type, api_key, job_id)
+    if os.path.exists(path):
+        shutil.rmtree(path)  # Remove previously created project
+    Path(path).mkdir(parents=True, exist_ok=True)
 
     io_bytes = io.BytesIO(fileobj.read())
     tar = tarfile.open(fileobj=io_bytes, mode='r')
-    tar.extractall(path=user_folder_path)
+    tar.extractall(path=path)
     tar.close()
 
-    destination = os.path.abspath(user_folder_path)
+    destination = os.path.abspath(path)
     logging.info(f"File saved to {destination}")
     return destination
 
