b6de8207a1f922b5774065ccb2af0798d3aef41d
Vadim Yanko <yanko.vadim@gmail.com>
2020-07-03 16:14:17 +0000

    Scheduler draft

--- job_executor/schedule.py
+++ job_executor/schedule.py
@@ -1,4 +1,36 @@
-# TODO: implement scheduler
+import os
+import json
+
+import boto3
+
+
+def schedule(cron_schedule: str, project_path: str) -> str:
+    events = boto3.client('events', region_name=os.getenv('AWS_REGION_NAME'))
+
+    result = events.put_rule(
+        Name=project_path,
+        ScheduleExpression=f"cron({cron_schedule})",  # TODO: convert default cron to AWS cron
+        State='ENABLED'
+    )
+    rule_arn = result['RuleArn']
+
+    res = events.put_targets(
+        Rule=project_path,
+        Targets=[
+            {
+                'Id': 'scheduled_projects_to_execute.fifo',
+                'Arn': 'arn:aws:sqs:us-east-1:202868668807:scheduled_projects_to_execute.fifo',
+                'Input': json.dumps({'project_path': project_path}),
+                'SqsParameters': {
+                    'MessageGroupId': 'project'  # TODO: figure our why do we need message group
+                }
+
+            }
+        ]
+    )
+    print(res)
+    return rule_arn  # TODO: store it somewhere
+
 
 # def generate_logs(api_key: str, logstream: Iterable[bytes], timestamp_str: str):
 #     cloud_watch = boto3.client('logs', region_name=os.getenv('AWS_REGION_NAME'))
