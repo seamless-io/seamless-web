8629fc9fa901bf6039bc98846eaaa3f6b5da9614
Vadim Yanko <yanko.vadim@gmail.com>
2020-07-04 10:34:02 +0000

    Proper SQS configuration

--- job_executor/schedule.py
+++ job_executor/schedule.py
@@ -15,14 +15,16 @@ def schedule(cron_schedule: str, project_path: str) -> str:
         ScheduleExpression=f"cron({cron_schedule})",  # TODO: convert default cron to AWS cron
         State='ENABLED'
     )
+    print(f'\n\nRule Results:\n {result}')
     rule_arn = result['RuleArn']
 
+    # TODO: create SQS in terraform
     res = events.put_targets(
         Rule=project_path,
         Targets=[
             {
-                'Id': 'scheduled_projects_to_execute.fifo',
-                'Arn': 'arn:aws:sqs:us-east-1:202868668807:scheduled_projects_to_execute.fifo',
+                'Id': 'scheduled-to-execute.fifo',
+                'Arn': 'arn:aws:sqs:us-east-1:202868668807:scheduled-to-execute.fifo',
                 'Input': json.dumps({'project_path': project_path}),
                 'SqsParameters': {
                     'MessageGroupId': 'project'  # TODO: figure our why do we need message group
@@ -31,7 +33,7 @@ def schedule(cron_schedule: str, project_path: str) -> str:
             }
         ]
     )
-    print(res)
+    print(f"\n\nPut Targets result:\n {res}")
     return rule_arn  # TODO: store it somewhere
 
 
