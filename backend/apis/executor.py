fdd84d11c6e64261a9973f260f71e4e3b97eee5e
Vadim Yanko <yanko.vadim@gmail.com>
2020-07-05 14:35:33 +0000

    reaname controller

--- backend/apis/executor.py
+++ backend/apis/executor.py
@@ -12,10 +12,11 @@ exec_bp = Blueprint('executor', __name__)
 
 # TODO: provide AUTH!
 @exec_bp.route('/execute', methods=['POST'])
-def execute_project():
+def execute_job():
     data = request.json()
     # TODO: programmatically tie the structure for data with one
     # provided in `job_executor/executor.py` file when writing to queue
     project_path = data['project_path']
+    print(f"Executing project {project_path}")
     # executor.execute_and_stream_to_db(project_path, ...)
-    return jsonify({}), 200
+    return jsonify(), 200
