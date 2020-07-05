3ae6caf8767fb8e4c9ab2ae78c00eaba5d11f514
Vadim Yanko <yanko.vadim@gmail.com>
2020-07-05 14:54:20 +0000

    Proper naming in scheduler

--- job_executor/scheduler.py
+++ job_executor/scheduler.py
@@ -28,7 +28,7 @@ def schedule(cron_schedule: str, job_id: str) -> str:
             {
                 'Id': QUEUE_ID,
                 'Arn': QUEUE_ARN,
-                'Input': json.dumps({'project_path': job_id}),
+                'Input': json.dumps({'job_id': job_id}),
                 'SqsParameters': {
                     'MessageGroupId': 'project'  # TODO: figure our why do we need message group
                 }
