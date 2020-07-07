1263c194ff71ebf00539861739494e2055d2a229
andheroe <freefunning@gmail.com>
2020-07-07 22:59:35 +0300

    created test endpoint for sns

--- backend/apis/sns/schedule_events.py
+++ backend/apis/sns/schedule_events.py
@@ -12,8 +12,9 @@ def run_job_by_schedule():
     logging.info(request.json)
     logging.info(request.args)
     logging.info(request.headers)
-    pprint.pformat(request.__dict__, depth=5)
-    # logging.info(request.text())
+    logging.info(request.get_data())
+    logging.info(request.content)
+    logging.info(request.text)
     return "Success", 200
     # job_id = request.json['job_id']
     # with session_scope() as db_session:
