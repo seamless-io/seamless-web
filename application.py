78c98ae2626cabc2ff5c8b1832f8ade5b1d6d9d4
andheroe <freefunning@gmail.com>
2020-07-09 16:21:22 +0300

    try this for branstalk

--- application.py
+++ application.py
@@ -4,8 +4,8 @@ from flask_socketio import SocketIO
 
 from backend.web import create_app
 
-app = create_app()
-socketio = SocketIO(app, async_mode='threading')
+application = create_app()
+socketio = SocketIO(application, async_mode='threading')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=os.environ.get('PORT', 5000))
+    socketio.run(application, host='0.0.0.0', port=os.environ.get('PORT', 5000))
