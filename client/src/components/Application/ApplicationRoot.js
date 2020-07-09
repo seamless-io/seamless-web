35b5739065f9fb9c04869023d580786b5734b48d
andheroe <freefunning@gmail.com>
2020-07-09 17:15:04 +0300

    added favicon.ico

--- client/src/components/Application/ApplicationRoot.js
+++ client/src/components/Application/ApplicationRoot.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Favicon from 'react-favicon';
 
 import { Container } from 'react-bootstrap';
 
 import './style.css';
+
+import favicon from '../../images/favicon.ico';
 
 import ApplicationHeader from './ApplicationHeader';
 import Jobs from '../Jobs/Jobs';
@@ -12,6 +15,7 @@ import JobDetails from '../Jobs/JobDetails/JobDetails';
 const ApplicationRoot = () => {
   return (
     <Router>
+      <Favicon url={favicon} />
       <ApplicationHeader />
 
       <Container fluid className="smls-main-container">
