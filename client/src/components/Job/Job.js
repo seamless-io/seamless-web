f11e2ac583b77a58ffe641704386192cdd6d4dac
Dima Ostapchuk <dima.ostapchuk@datarobot.com>
2020-07-10 17:17:07 +0300

    add logs sections

--- client/src/components/Job/Job.js
+++ client/src/components/Job/Job.js
@@ -7,6 +7,7 @@ import { getJob } from '../../api';
 
 import './style.css';
 import download from '../../images/cloud-download.svg';
+import timeHistory from '../../images/time-history.svg';
 
 const Job = () => {
   const job = useParams();
@@ -40,6 +41,44 @@ const Job = () => {
               </span>
             </button>
           </div>
+        </Col>
+      </Row>
+      <Row className="smls-job-extra-info">
+        <Col style={{ paddingLeft: '0px' }}>
+          <div className="smls-job-extra-info-section">
+            <span>Schedule</span>
+          </div>
+        </Col>
+        <Col style={{ paddingRight: '0px' }}>
+          <div className="smls-job-extra-info-section">
+            <img src={timeHistory} alt="Updated at" />
+            <span>Code updated on May 19, 2020, 14:56</span>
+          </div>
+        </Col>
+      </Row>
+      <Row className="smls-job-main-info">
+        <Col
+          sm={4}
+          className="smls-job-main-info-section"
+          style={{ borderRight: '2px solid #ebedf0' }}
+        >
+          <Row>
+            <Col>
+              <h5 className="smls-job-main-info-section-header">
+                Execution Timeline
+              </h5>
+            </Col>
+          </Row>
+        </Col>
+        <Col sm={8} className="smls-job-main-info-section">
+          <Row>
+            <Col sm={12}>
+              <h5 className="smls-job-main-info-section-header">Logs</h5>
+            </Col>
+            <Col sm={12}>
+              <div className="smls-job-container">logs....</div>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </>
