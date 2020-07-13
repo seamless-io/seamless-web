49d489a1b173f9f026fdb09d7a8904636ae2a78c
Dima Ostapchuk <dima.ostapchuk@datarobot.com>
2020-07-13 17:51:45 +0300

    add styles to no jobs page

--- client/src/components/Jobs/Jobs.js
+++ client/src/components/Jobs/Jobs.js
@@ -8,6 +8,8 @@ import JobLine from './JobLine';
 import './style.css';
 
 import linkExternal from '../../images/link-external.svg';
+import jobsLogo from '../../images/lightning.svg';
+import terminal from '../../images/terminal.svg';
 
 const Jobs = () => {
   const [jobs, setjobs] = useState([]);
@@ -17,7 +19,9 @@ const Jobs = () => {
     getJobs()
       .then(payload => {
         setjobs(payload);
-        setNoJobsStyle(payload && payload.length > 0 ? '' : 'smls-no-jobs');
+        setNoJobsStyle(
+          payload && payload.length > 0 ? '' : 'smls-no-jobs-header'
+        );
       })
       .catch(() => {
         alert('Error!');
@@ -44,14 +48,38 @@ const Jobs = () => {
     return (
       <>
         <Row className="smls-no-jobs">
-          <Col className="smls-no-jobs-message">
-            <div>You have no jobs.</div>
-            <div>
-              To proceed with seamless - install & configure seamless CLI.
+          <Col className="smls-no-jobs-icon-container">
+            <div className="smls-no-jobs-icon blue">
+              <img src={jobsLogo} className="smls-jobs" alt="Jobs"></img>
+            </div>
+            <div className="smls-no-jobs-message">
+              <div>
+                <strong>You have no jobs</strong>
+              </div>
+              <div>
+                To proceed with seamless - install & configure seamless CLI.
+              </div>
             </div>
           </Col>
         </Row>
-        <Row></Row>
+        <Row className="smls-no-jobs">
+          <Col className="smls-no-jobs-icon-container">
+            <div className="smls-no-jobs-icon green">
+              <img src={terminal} className="smls-jobs" alt="Terminal"></img>
+            </div>
+            <div className="smls-no-jobs-message">
+              <p>
+                Installation Guide<sup>*</sup>
+              </p>
+              <p>
+                Copy this into your terminal, run the command, and follow
+                further instractions:
+              </p>
+              <p>pip install seamless-cli</p>
+              <p>* Requires Python 3.6 or higher</p>
+            </div>
+          </Col>
+        </Row>
       </>
     );
   };
