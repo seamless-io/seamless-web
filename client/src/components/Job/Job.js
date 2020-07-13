5f3e1083c2be5830d658f90d642317d69c783d80
Dima Ostapchuk <dima.ostapchuk@datarobot.com>
2020-07-13 23:36:26 +0300

    add spinner

--- client/src/components/Job/Job.js
+++ client/src/components/Job/Job.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
-import { Row, Col } from 'react-bootstrap';
+import { Row, Col, Spinner } from 'react-bootstrap';
 import Toggle from 'react-toggle';
 
 import { getJob } from '../../api';
@@ -15,18 +15,37 @@ import pencil from '../../images/pencil-create.svg';
 const Job = () => {
   const job = useParams();
   const [name, setName] = useState('');
+  const [schedule, setSchedule] = useState('');
   const [isScheduleOn, setIsScheduleOn] = useState(false);
   const [isToggleDisabled, setIsToggleDisabled] = useState(true);
+  const [scheduleClassName, setScheduleClassName] = useState('');
+  const [loading, setLoading] = useState(null);
 
   useEffect(() => {
+    setLoading(true);
     getJob(job.id)
       .then(payload => {
         setName(payload.name);
+
+        if (payload.schedule === 'None') {
+          setSchedule('Not scheduled');
+          setScheduleClassName('smls-muted');
+        }
+
+        setLoading(false);
       })
       .catch(() => {
         alert('Error!');
       });
   }, []);
+
+  if (loading) {
+    return (
+      <div className="smls-jobs-spinner-container">
+        <Spinner animation="border" role="status"></Spinner>
+      </div>
+    );
+  }
 
   return (
     <>
@@ -68,9 +87,10 @@ const Job = () => {
             <Toggle
               defaultChecked={isScheduleOn}
               icons={false}
+              disabled={isToggleDisabled}
               onChange={() => alert('Not working yet.')}
             />
-            <span>Scheduled for every Monday, at 12 PM</span>
+            <span className={scheduleClassName}>{schedule}</span>
           </div>
         </Col>
         <Col style={{ paddingRight: '0px' }}>
