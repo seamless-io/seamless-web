d861a5f01655e193735fd2098c5870a34a1fcc0f
andheroe <freefunning@gmail.com>
2020-07-14 16:46:26 +0300

    Disable information logs from socketio, including PING/PONG logs

--- application.py
+++ application.py
@@ -1,3 +1,4 @@
+import logging
 import os
 
 from flask_socketio import SocketIO
@@ -6,6 +7,10 @@ from backend.web import create_app
 
 # The name of this variable (as well as this file) is important for Beanstalk
 application = create_app()
+
+# Disable information logs from socketio, including PING/PONG logs
+logging.getLogger('socketio').setLevel(logging.ERROR)
+logging.getLogger('engineio').setLevel(logging.ERROR)
 socketio = SocketIO(application, async_mode='threading')
 
 if __name__ == '__main__':
