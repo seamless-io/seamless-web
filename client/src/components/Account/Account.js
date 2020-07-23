d09001d9085471163809625853f30c5d4dac592c
Dima Ostapchuk <dima.ostapchuk@datarobot.com>
2020-07-23 23:07:11 +0300

    delete not working buttons

--- client/src/components/Account/Account.js
+++ client/src/components/Account/Account.js
@@ -3,8 +3,6 @@ import { Row, Col, Spinner } from 'react-bootstrap';
 
 import { getUserInfo } from '../../api';
 
-import reset from '../../images/rotate-left.svg';
-import copy from '../../images/copy.svg';
 import key from '../../images/key.svg';
 import atSign from '../../images/at-sign.svg';
 import './style.css';
@@ -27,22 +25,10 @@ const Account = () => {
       });
   }, []);
 
-  const copyApiKey = () => {
-    alert('Not working yet');
-  };
-
-  const resetApiKey = () => {
-    alert('Not working yet');
-  };
-
-  const changePassword = () => {
-    alert('Not working yet');
-  };
-
   if (loading) {
     return (
       <div className="smls-jobs-spinner-container">
-        <Spinner animation="border" role="status"></Spinner>
+        <Spinner animation="border" role="status" />
       </div>
     );
   }
@@ -56,112 +42,28 @@ const Account = () => {
           </Col>
         </Row>
       </div>
-      <div className="smls-account-key smls-card">
+      <div className="smls-card">
         <Row>
           <Col sm={12} className="smls-account-section-header-container">
             <h5 className="smls-account-section-header">API Key</h5>
           </Col>
-          <Col sm={7}>
+          <Col sm={12}>
             <div className="smls-account-section-text">
               <img src={key} className="smls-account-key" alt="Api key" />
               <span>{apiKey}</span>
             </div>
           </Col>
-          <Col sm={2}>
-            <div className="smls-account-copy-container">
-              <button className="smls-account-copy-button" onClick={copyApiKey}>
-                <img src={copy} className="smls-account-copy" alt="Copy" />
-                <span className="smls-account-copy-button-text">Copy</span>
-              </button>
-            </div>
-          </Col>
-          <Col sm={3}>
-            <div className="smls-account-reset-key-container">
-              <button
-                className="smls-account-reset-key-button"
-                onClick={resetApiKey}
-              >
-                <img
-                  src={reset}
-                  className="smls-account-reset"
-                  alt="Reset key"
-                />
-                <span className="smls-account-reset-button-text">
-                  Reset Key
-                </span>
-              </button>
-            </div>
-          </Col>
         </Row>
       </div>
-      <div className="smls-account-email smls-card">
+      <div className="smls-card">
         <Row>
           <Col sm={12} className="smls-account-section-header-container">
             <h5 className="smls-account-section-header">My Email</h5>
           </Col>
-          <Col sm={7}>
+          <Col sm={12}>
             <div className="smls-account-section-text">
               <img src={atSign} className="smls-account-key" alt="Api key" />
               <span>{email}</span>
-            </div>
-          </Col>
-        </Row>
-      </div>
-      <div className="smls-account-password smls-card">
-        <Row>
-          <Col sm={12} className="smls-account-section-header-container">
-            <h5 className="smls-account-section-header">Change Password</h5>
-          </Col>
-          <Col sm={7} className="smls-account-input-contaier">
-            <div>
-              <label
-                htmlFor="old-password"
-                className="smls-account-input-label"
-              >
-                Old Password
-              </label>
-              <input className="smls-account-input" id="old-password"></input>
-            </div>
-          </Col>
-          <Col sm={7} className="smls-account-input-contaier">
-            <div>
-              <label
-                htmlFor="new-password"
-                className="smls-account-input-label"
-              >
-                New Password
-              </label>
-              <input
-                className="smls-account-input"
-                id="new-password"
-                placeholder="min. 8 characters"
-              ></input>
-            </div>
-          </Col>
-          <Col sm={7} className="smls-account-input-contaier">
-            <div>
-              <label
-                htmlFor="confirm-password"
-                className="smls-account-input-label"
-              >
-                Confirm New Password
-              </label>
-              <input
-                className="smls-account-input"
-                id="confrim-password"
-              ></input>
-            </div>
-          </Col>
-          <Col sm={7} className="smls-account-input-contaier">
-            <div>
-              <button
-                className="smls-account-change-password-button"
-                onClick={changePassword}
-              >
-                <span className="smls-account-change-password-button-text">
-                  Change Password
-                </span>
-              </button>
             </div>
           </Col>
         </Row>
