0209fee7ebde7c12191819f393ea3765be92652b
andheroe <freefunning@gmail.com>
2020-07-23 19:29:53 +0300

    move file size limit to NGINX

--- app_config.py
+++ app_config.py
@@ -4,7 +4,6 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'hard to guess string')
     LOGIN_DISABLED = os.environ.get('LOGIN_DISABLED', False)
-    MAX_CONTENT_LENGTH = 10 * 1024 * 1024  # 10 Mb file size limit
 
     @staticmethod
     def init_app(app):
