94857530ceadc0c3bf121d28395e210340261dbc
Dima Ostapchuk <dima.ostapchuk@datarobot.com>
2020-07-24 10:45:22 +0300

    add styles to header

--- client/src/components/Application/ApplicationHeader.js
+++ client/src/components/Application/ApplicationHeader.js
@@ -6,7 +6,9 @@ import './style.css';
 import logo from '../../images/seamless-logo-black.svg';
 import logout from '../../images/circle-arrow-right.svg';
 import userAccount from '../../images/user.svg';
+import userAccountInactive from '../../images/user-inactive.svg';
 import jobsLogo from '../../images/lightning.svg';
+import jobsLogoInactive from '../../images/lightning-inactive.svg';
 
 const ApplicationHeader = () => {
   return (
@@ -18,19 +20,43 @@ const ApplicationHeader = () => {
           </Navbar.Brand>
         </Col>
         <Col className="smls-header-center">
-          <Nav.Link href="/">
-            <img src={jobsLogo} className="smls-jobs" alt="Jobs" />
-            Jobs
-          </Nav.Link>
-          <Nav.Link href="/account">
-            <img src={userAccount} className="smls-user" alt="User account" />
-            My Account
-          </Nav.Link>
+          <div
+            className={`smls-header-jobs-container ${
+              location.pathname === '/' ? 'smls-header-active' : ''
+            }`}
+          >
+            <Nav.Link href="/" className="smls-header-link">
+              <img
+                src={location.pathname === '/' ? jobsLogo : jobsLogoInactive}
+                className="smls-jobs"
+                alt="Jobs"
+              />
+              Jobs
+            </Nav.Link>
+          </div>
+          <div
+            className={`smls-header-jobs-container ${
+              location.pathname === '/account' ? 'smls-header-active' : ''
+            }`}
+          >
+            <Nav.Link href="/account" className="smls-header-link">
+              <img
+                src={
+                  location.pathname === '/account'
+                    ? userAccount
+                    : userAccountInactive
+                }
+                className="smls-user"
+                alt="User account"
+              />
+              My Account
+            </Nav.Link>
+          </div>
         </Col>
         <Col className="smls-header-logout">
           <Nav.Link
-            href={window.location.origin + '/logout'}
-            className="smls-header-right"
+            href={`${window.location.origin}/logout`}
+            className="smls-header-right smls-header-link"
           >
             <img src={logout} className="smls-logout" alt="Logout" />
             Logout
