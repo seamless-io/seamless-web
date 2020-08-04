eeb404deb6133fc3359200369c02723d99d3d07a
finomayato <vadim.yanko@datarobot.com>
2020-08-04 16:14:19 +0300

    Add function wich raises an error

--- tests/test_project/main_module.py
+++ tests/test_project/main_module.py
@@ -3,6 +3,9 @@ import requests
 
 def read_news():
     res = requests.get('https://en.wikipedia.org/wiki/List_of_news_media_APIs')
-    for line in res.content:
-        print(line)
+    print(res.text)
     return "Everything is alright"
+
+
+def exception_function():
+    raise RuntimeError('This is a dummy exception')
