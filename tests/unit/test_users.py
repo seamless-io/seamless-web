4fda33f64e8d8345babfd0a5935db0c15f9db136
finomayato <vadim.yanko@datarobot.com>
2020-08-24 12:09:19 +0300

    Skeleton for future tests

---
+++ tests/unit/test_users.py
@@ -0,0 +1,8 @@
+import pytest
+
+
+@pytest.mark.skip(reason="Implement the test")
+def test_users_register_several_times():
+    # sentry issue
+    # https://sentry.io/organizations/seamless-cloud/issues/1788486965/?project=5339513&query=is%3Aunresolved
+    pass
