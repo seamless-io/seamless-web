60459467bc2c5ff4bc30cfb1c7d04dc2ac234ea6
andheroe <freefunning@gmail.com>
2020-08-24 11:26:00 +0300

    move load test to a different folder

--- tests/integration/conftest.py
+++ tests/integration/conftest.py
@@ -1,33 +1,7 @@
 import uuid
-from unittest.mock import patch
 
 import docker
 import pytest
-
-from core.api_key import generate_api_key
-from core.models import session_scope
-from core.models.users import User
-from core.helpers import row2dict
-
-NUMBER_OF_TEST_USERS = 2
-DB_PREFIX = "INTEGRATION_TEST"
-
-
-@pytest.fixture(scope="session")
-def test_users():
-    with patch('core.db.DB_PREFIX', DB_PREFIX):
-        with session_scope() as db_session:
-            users = [User(email=f"{i}@loadtest.com", api_key=generate_api_key())
-                     for i in range(NUMBER_OF_TEST_USERS)]
-            for user in users:
-                db_session.add(user)
-            db_session.commit()
-
-            yield [row2dict(user) for user in users]
-
-            for user in users:
-                db_session.delete(user)
-            db_session.commit()
 
 
 # @pytest.fixture(scope='session')
