e9c1915aa7fa56aee9305da55e4e83574f5be580
andheroe <freefunning@gmail.com>
2020-09-02 10:05:19 +0300

    add an unsubsribed list to product updates script

--- scripts/send_product_update.py
+++ scripts/send_product_update.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 from core.emails.client import send_email
 from core.models import get_db_session, User
 
+unsubsribed_list = ['ctapbiu.spb@gmail.com',
+                    'hubermar4@gmail.com']
+
 load_dotenv()
 
 users = get_db_session().query(User).all()
@@ -12,9 +15,10 @@ users = get_db_session().query(User).all()
 with open('<name of html file with update>', 'r') as file:
     content = file.read()
 for user in users:
-    print(user.email)
-    send_email(user.email,
-               'Seamless Cloud Product Update <date>',
-               content)
-    sleep(1)
+    if user.email not in unsubsribed_list:
+        print(user.email)
+        send_email(user.email,
+                   'Seamless Cloud Product Update <date>',
+                   content)
+        sleep(1)
 
