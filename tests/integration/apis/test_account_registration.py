2547358f08e9116f1401947d814e6a1c78221b07
finomayato <vadim.yanko@datarobot.com>
2020-09-17 00:10:47 +0300

    Create registration mockups

---
+++ tests/integration/apis/test_account_registration.py
@@ -0,0 +1,50 @@
+
+
+def test_free():
+    """
+    When user is registered with using "Basic" free plan or default "Sign Up" next things should happen:
+    * Personal Team (workspace) should be created
+    * Registered user should be a part of this team
+    """
+    pass
+
+
+def test_startup():
+    """
+    When user is registered with "Startup":
+    * Extra fields while registration pop-up: "Team Name"
+    * Personal Workspace created
+    * {Team name} workspace created
+    * Registered user becomes part of Personal Workspace and {Team name} workspace
+    * User become and owner (the one who is paying for a team)
+    * Private Job Templates initialized (still TODO)
+    """
+    pass
+
+
+def test_business():
+    """
+    When user is registered with "Business":
+    * Extra fields while registration pop-up: "Team Name"
+    * Personal Workspace created
+    * {Team name} workspace created
+    * Registered user becomes part of Personal Workspace and {Team name} workspace
+    * User become and owner (the one who is paying for a team)
+    * Private Job Templates initialized (still TODO)
+    * 4 office hours with us
+    """
+    pass
+
+
+def test_enterprise():
+    """
+    When user is registered with "Enterprize":
+    * Extra fields while registration pop-up: "Team Name"
+    * Personal Workspace created
+    * {Team name} workspace created
+    * Registered user becomes part of Personal Workspace and {Team name} workspace
+    * User become and owner (the one who is paying for a team)
+    * Private Job Templates initialized (still TODO)
+    * 4 office hours with us
+    """
+    pass
