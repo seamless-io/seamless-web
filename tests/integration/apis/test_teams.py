4634e6d8c0f14a3fba9bc8506ae7bf733d5361bd
finomayato <vadim.yanko@datarobot.com>
2020-09-17 00:13:13 +0300

    Team CRUD operations tests

---
+++ tests/integration/apis/test_teams.py
@@ -0,0 +1,28 @@
+
+
+def create_team():
+    """
+    Already registered user creates a team
+    """
+    pass
+
+
+def delete_team():
+    """
+    User deletes a team
+    """
+    pass
+
+
+def invite_user():
+    """
+    Owner invites a user to the team
+    """
+    pass
+
+
+def remove_user():
+    """
+    Owner removes user from a team
+    """
+    pass
