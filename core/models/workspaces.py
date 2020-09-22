1cb7fa205a2ff26365cd32dea8fb76fb82f8d8f5
finomayato <vadim.yanko@datarobot.com>
2020-09-23 00:23:48 +0300

    Create invitation model

--- core/models/workspaces.py
+++ core/models/workspaces.py
@@ -1,9 +1,11 @@
 import datetime
 import enum
+import uuid
 from dataclasses import dataclass
 
 from sqlalchemy import Column, Integer, Text, Boolean, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
+from sqlalchemy.dialects.postgresql import UUID
 
 from core.models.base import base
 
@@ -43,3 +45,24 @@ class Workspace(base):
 
     def __repr__(self):
         return '<Workspace %r %r %r>' % (self.id, self.name, self.plan)
+
+
+class InvitationStatus(enum.Enum):
+    pending = "pending"
+    accepted = "accepted"
+
+
+class Invitation(base):
+    __tablename__ = 'invitations'
+
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+
+    user_id = Column(Integer)
+    workspace_id = Column(Integer)
+
+    status = Column(Text, nullable=False)
+
+    created_at = Column(DateTime, default=datetime.datetime.utcnow)
+
+    def __repr__(self):
+        return f"<Invitation(user_id={self.user_id}, workspace_id={self.workspace_id})>"
